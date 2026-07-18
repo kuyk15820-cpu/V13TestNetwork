b8b1892b1d0588ac917ead77ca0817589584c17a
kuyk15820-cpu <kuyk15820@gmail.com>
2026-07-18 23:34:06 +0700

    Update RootViewController.mm

--- sources/RootViewController.mm
+++ sources/RootViewController.mm
@@ -479,7 +479,8 @@
             [FTNotificationIndicator setNotificationIndicatorStyle:UIBlurEffectStyleDark];
             [FTNotificationIndicator showNotificationWithImage:successIcon
                                                           title:[NSString stringWithUTF8String:AY_OBFUSCATE("ดาวน์โหลดเสร็จสิ้น")]
-                                                        message:[NSString stringWithUTF8String:AY_OBFUSCATE("กรุณาบันทึกไฟล์ .ipa เพื่อติดตั้งเวอร์ชันล่าสุด")]
+
+message:[NSString stringWithUTF8String:AY_OBFUSCATE("กรุณาบันทึกไฟล์ .ipa เพื่อติดตั้งเวอร์ชันล่าสุด")]];
             
             // เรียกแชร์เปิดไฟล์ผ่านระบบ Share Sheet ทันที เพื่อให้ผู้ใช้เลือก Save to Files หรือติดตั้งเอง
             NSArray *itemsToShare = @[destinationURL];
